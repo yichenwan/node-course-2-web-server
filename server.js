bd9bb99717c4a695d0972a20be05da39ee5eeeb3
carrie <carriewan7@gamail.com>
2018-11-27 11:29:57 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require(`express`);
+const hbs = require(`hbs`);
+const fs = require(`fs`);
+
+var app = express();
+
+hbs.registerPartials(__dirname + `/views/partials`);
+app.set(`view engine`, hbs);
+app.use(express.static(__dirname + `/public`));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile(`server.log`, log + `\n`, (err) => {
+    if (err) {
+      console.log(`Unable to append to server.log.`)
+    }
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render(`maintenance.hbs`);
+});
+
+hbs.registerHelper(`getCurrentYear`, () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper(`screamIt`, (text) => {
+  return text.toUpperCase();
+});
+
+app.get(`/`, (req, res) => {
+  res.render(`home.hbs`, {
+    pageTitle: `Home Page`,
+    welcomeMessage: `Welcome to home page`,
+  });
+});
+
+app.get(`/about`, (req, res) => {
+  res.render(`about.hbs`, {
+    pageTitle: `About Page`,
+  });
+});
+
+// /bad
+
+app.get(`/bad`, (req, res) => {
+  res.send({
+    errorMessage: `error handling request`
+  })
+});
+
+app.listen(3000, () => {
+  console.log(`Server is up on port 3000`);
+});
